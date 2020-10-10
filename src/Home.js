3967a6708b47c1c1ea02e2463f7c43d17d2a8192
pashyaa <pashyaa.technologies@gmail.com>
2020-10-11 00:27:59 +0530

    ui developer react assignment

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Home extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>React Assignment</h1>
+        <p>
+          Home Page
+        </p>
+      </div>
+    );
+  }
+}
+export default Home;
